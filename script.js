610f59b97f6d134998281187b43e0bc9d0142ce9
Numan <numaniftikhar300@gmail.com>
2022-09-29 14:32:49 +0500

    add selected rating to span func

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 // get cards to hide and show on submit click
-const ratingCard = document.querySelector(".ratingCard")
-const thankYouCard = document.querySelector(".thankYouCard")
-const submitBtn = document.querySelector(".submitBtn")
+const ratingCard = document.querySelector(".ratingCard");
+const thankYouCard = document.querySelector(".thankYouCard");
+const submitBtn = document.querySelector(".submitBtn");
+const inputs = document.getElementsByTagName("input");
+const ratingSpan = document.querySelector(".selectedRating");
 
-
-submitBtn.addEventListener('click', ()=>{
-    ratingCard.classList.add('hidden')
-    thankYouCard.classList.remove('hidden')
-})
+submitBtn.addEventListener("click", () => {
+  for (let i = 0; i < inputs.length; i++) {
+    const element = inputs[i];
+    if (element.checked) ratingSpan.innerHTML = element.value;
+  }
+  ratingCard.classList.add("hidden");
+  thankYouCard.classList.remove("hidden");
+});
